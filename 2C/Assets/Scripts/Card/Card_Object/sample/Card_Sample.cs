7c4bb1fa1777c2c129cca7de3d8714b6e3570a0f
Kobaysh <73707817+Kobaysh@users.noreply.github.com>
2022-06-03 10:32:42 +0900

    add:card_object

---
+++ 2C/Assets/Scripts/Card/Card_Object/sample/Card_Sample.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+ [CreateAssetMenu(menuName = "Assets/Card Scriptable/Card_Sample")]
+public class Card_Sample : Card_Abstract {
+ 
+    // static field
+
+    // public member
+
+    // serialized field
+
+    // private member
+
+    public override void Awake() {
+        
+    }
+
+    public override void Start () {
+	
+	}
+	
+
+    public override void Update () {
+
+	}
+
+	// Action
+	public override void Action()
+	{
+
+	}
+
+}
