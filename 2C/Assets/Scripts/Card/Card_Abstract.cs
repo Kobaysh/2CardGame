c7d7ffca16bd956a753af87e1818342e90942e8d
Kobaysh <73707817+Kobaysh@users.noreply.github.com>
2022-06-03 10:49:07 +0900

    add:card type

--- 2C/Assets/Scripts/Card/Card_Abstract.cs
+++ 2C/Assets/Scripts/Card/Card_Abstract.cs
@@ -7,12 +7,20 @@ public abstract class Card_Abstract : ScriptableObject
     // static field
 
     // public member
+    public enum Card_Type
+    {
+        Attack,
+        Skill,
+        Power
+    }
 
     // serialized field
     [SerializeField]
     protected string cardName;
     [SerializeField]
     protected int cost;
+    [SerializeField]
+    protected Card_Type cardType;
     [SerializeField]
     protected string cardText;
     [SerializeField]
