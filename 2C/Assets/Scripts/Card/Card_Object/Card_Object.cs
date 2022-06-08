99191c2512e4108a46efe8a27c6e0004b8bd7360
Kobaysh <73707817+Kobaysh@users.noreply.github.com>
2022-06-08 16:38:59 +0900

    add:cardobj

---
+++ 2C/Assets/Scripts/Card/Card_Object/Card_Object.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Card
+{
+    public class Card_Object : MonoBehaviour
+    {
+
+        // static field
+
+        // public member
+        public Card_Abstract havingCard;
+        // serialized field
+        
+        // private member
+
+        public void Awake()
+        {
+
+        }
+
+        public void Start()
+        {
+
+        }
+
+
+        public void Update()
+        {
+
+        }
+
+        public void FixedUpdate()
+        {
+
+        }
+    }
+}
